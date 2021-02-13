cc000205b9cdc171440c9b3a8a37f9140951cfe6
liangyuzhu1991 <Liangyu@Liangyu.attlocal.net>
2021-02-13 15:48:39 -0800

    Modified verbs

--- generator.js
+++ generator.js
@@ -1,6 +1,6 @@
 const sentences={
     subjects:['faith','pet','lover','partner','friend','family','career','hobby'],
-    verbs:['takes','runs','gets','spends','throws','brings','owns','represent','generates'],
+    verbs:['takes away','increases','decreases','leads to','brings','owns','represents','generates'],
     objects:['Love','Wealth','Fortune','Freedom','Hatred','Happiness','Silence','Calm',
     'Desire','Alarm','Success','Failure','Sadness','Danger','Risk','Challenge','Confidence','Fun'],
     random_subject: function(){
@@ -13,7 +13,7 @@ const sentences={
         return this.objects[Math.floor(Math.random()*this.objects.length)]
     },
     generate_sentence: function(){
-        console.log(`Your ${this.random_subject()} ${this.random_verb} ${this.random_object()}.`)
+        console.log(`Your ${this.random_subject()} ${this.random_verb()} ${this.random_object()}.`)
     }
 }
 sentences.generate_sentence()
